b6cae763c55280cf268188dcba235dad28d189eb
ahmad.kheder <ahmad.kheder@carmy.ai>
2024-12-31 01:01:46 +0100

    fixes

--- youtube-downloader/backend/app.py
+++ youtube-downloader/backend/app.py
@@ -56,4 +56,4 @@ def download_audio():
         return jsonify({"success": False, "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
