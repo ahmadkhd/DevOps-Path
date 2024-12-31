d2179763f61647968d851e11225ceddf4c226c58
ahmad.kheder <ahmad.kheder@carmy.ai>
2024-12-31 01:33:04 +0100

    added flask-cors

--- youtube-downloader/backend/app.py
+++ youtube-downloader/backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import subprocess
-import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Route to serve the frontend (index.html)
 @app.route('/')
